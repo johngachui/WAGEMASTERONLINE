c11e3c4428412b1bfc84736e302bab1fae1f62f8
John Gachui <johngachui@hotmail.com>
2024-03-16 22:57:29 +0300

    ASGI server and FastAPI set up

--- wagemaster_online/wagemaster_online/asgi.py
+++ wagemaster_online/wagemaster_online/asgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+from fastapi import FastAPI
+from django.core.asgi import get_asgi_application
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wagemaster_online.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+fastapi_app = FastAPI()
+
+@fastapi_app.get("/hello")
+async def read_fastapi():
+    return {"message": "Hello from FastAPI"}
+
+from starlette.routing import Mount, Route
+from starlette.applications import Starlette
+
+application = Starlette(routes=[
+    Mount("/api", app=fastapi_app),
+    Mount("/", app=django_asgi_app),
+])
